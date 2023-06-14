19c4857b7cd082c79e5a48052450aa5047397c22
sakshii2004 <124381306+sakshii2004@users.noreply.github.com>
2023-06-14 09:08:48 +0000

    Added Job information, added nav bar and footer.

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {'id': 1, 'title': 'Data Analyst','location':'Bengaluru, India', 'salarly': 'Rs 10,00,000'
+  },
+  {'id': 2, 'title': 'Data Scientist','location':'Delhi', 'salarly': 'Rs 12,00,000'
+  },
+  {'id': 3, 'title': 'Frontend Engineer','location':'Remote', 'salarly': 'Rs 8,00,000'
+  },
+  {'id': 4, 'title': 'Backend Engineer','location':'San Francisco', 'salarly': '$120,000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 if __name__=='__main__':
   app.run(host='0.0.0.0', debug=True)
